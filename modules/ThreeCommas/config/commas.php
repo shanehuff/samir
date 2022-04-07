53aad0166e18eb8089220dfba7bc6b246ca1b26b
Nguyen Ha Duy <duynguyen@okxe.vn>
2022-04-07 15:28:53 +0700

    SM-5 Update config document #6

--- modules/ThreeCommas/config/commas.php
+++ modules/ThreeCommas/config/commas.php
@@ -14,6 +14,14 @@ return [
 	| A basic uri of api
 	*/
     'base_uri' => env('3COMMAS_BASE_ENDPOINT', 'https://api.3commas.io/public/api'),
+	
+    /*
+	|------------------------------------------------------------------------------
+	| 3Commas API Key and secret
+	|------------------------------------------------------------------------------
+	|
+	| Login and create a key/secret pair in https://3commas.io/api_access_tokens
+	*/
 	'api_key' => env('3COMMAS_API_KEY', ''),
 	'secret_key' => env('3COMMAS_SECRET_KEY', '')
 ];
