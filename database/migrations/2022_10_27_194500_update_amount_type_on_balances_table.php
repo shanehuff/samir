d03dccba7642faedf75438c6a1678228b661924a
Son Do <son@chillbits.com>
2022-10-27 12:48:05 +0000

    Update amount type

---
+++ database/migrations/2022_10_27_194500_update_amount_type_on_balances_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('balances', function (Blueprint $table) {
+            $table->double('available')->change();
+            $table->double('on_order')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('balances', function (Blueprint $table) {
+            $table->unsignedDouble('available')->change();
+            $table->unsignedDouble('on_order')->change();
+        });
+    }
+};
