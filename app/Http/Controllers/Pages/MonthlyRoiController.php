9d43e5e20e51d716ce501d99d53da8c43d252401
Shane Huff <86779613+shanehuff@users.noreply.github.com>
2023-01-12 12:42:35 +0700

    Update MonthlyRoiController.php

--- app/Http/Controllers/Pages/MonthlyRoiController.php
+++ app/Http/Controllers/Pages/MonthlyRoiController.php
@@ -26,6 +26,7 @@ class MonthlyRoiController
             ->with('profit')
             ->whereHas('profit', function ($query) {
                 $query->where('net_profit', '>', 0);
+                $query->where('id', '>=', 101);
             })
             ->orderByDesc('updated_at')
             ->get();
@@ -40,7 +41,7 @@ class MonthlyRoiController
                 $monthlyProfits->push((object)[
                     'net_profit' => $this->toVND($deal->sum('profit.net_profit')),
                     'month' => $deal->first()->updated_at->format('m/Y'),
-                    'roi' => number_format($deal->sum('profit.net_profit') / 63.0562 * 100, 2)
+                    'roi' => number_format($deal->sum('profit.net_profit') / 20 * 100, 2)
                 ]);
             }
         });
