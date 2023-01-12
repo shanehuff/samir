f00789697d58a43822607c820fe7f3f8a46afe5e
Shane Huff <86779613+shanehuff@users.noreply.github.com>
2023-01-12 12:37:31 +0700

    Update DealsController.php

--- app/Http/Controllers/Pages/DealsController.php
+++ app/Http/Controllers/Pages/DealsController.php
@@ -24,6 +24,7 @@ class DealsController
         $deals = Dealer::with('profit')
             ->whereHas('profit', function ($query) {
                 $query->where('net_profit', '>', 0);
+                $query->where('id', '>=', 101);
             })
             ->orderByDesc('created_at')
             ->limit(20)
