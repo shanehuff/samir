25180f007025405d4d29f84c0a0b3a25c4b170ff
Shane Huff <86779613+shanehuff@users.noreply.github.com>
2023-01-13 12:02:00 +0700

    Update DashboardController.php

--- app/Http/Controllers/Pages/DashboardController.php
+++ app/Http/Controllers/Pages/DashboardController.php
@@ -21,7 +21,7 @@ class DashboardController
 
     public function __invoke(Request $request): Response
     {
-        $profits = DealerProfit::where('id', '>=', 101)->get();
+        $profits = DealerProfit::all(); // where('id', '>=', 101)->get()
 
         return Jetstream::inertia()->render($request, 'Dashboard/Show', [
             'netProfit' => $this->toVND($profits->sum('net_profit')),
