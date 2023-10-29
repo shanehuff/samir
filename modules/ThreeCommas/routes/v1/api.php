5c07b850eceefc324371a22b3594d52737fee315
Son Do <son@chillbits.com>
2023-10-29 14:04:41 +0000

    Remove unused module

--- modules/ThreeCommas/routes/v1/api.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\ThreeCommas\Http\Controllers\V1\BotController;
-
-Route::get('/bots/{id}', [BotController::class, 'show'])->name('bots.show');
-Route::get('/bots', [BotController::class, 'index'])->name('bots.index');
